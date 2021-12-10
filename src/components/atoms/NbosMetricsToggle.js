ca7ca5f747f15df78eba84bf5c047ae6530395f8
shartsoe <shartsoe@insight.com>
2021-12-10 10:43:09 -0500

    built metrics toggle

---
+++ src/components/atoms/NbosMetricsToggle.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './togglestyles.css';
+import SwitchSelector from 'react-switch-selector';
+
+const options = [
+  {
+    label: 'Outcome Metrics',
+    value: 'outcome',
+    selectedBackgroundColor: '#ffffff',
+    innerHeight: 50,
+    selectedFontColor: '#0166CC',
+  },
+  {
+    label: 'Behavior Metrics',
+    value: 'behavior',
+    selectedBackgroundColor: '#ffffff',
+    selectedFontColor: '#0166CC',
+  },
+];
+
+const onChange = newValue => {
+  console.log(newValue);
+};
+
+const initialSelectedIndex = options.findIndex(({ value }) => value === 'bar');
+
+export const NbosMetricsToggle = () => {
+  return (
+    <div className="toggle">
+      <SwitchSelector
+        onChange={onChange}
+        options={options}
+        initialSelectedIndex={initialSelectedIndex}
+        backgroundColor={'#0166CC'}
+        fontColor={'#f5f6fa'}
+        wrapperBorderRadius={3}
+        optionBorderRadius={2}
+      />
+    </div>
+  );
+};
--- src/stories/atoms/NbosMetricsToggle.stories.js
+++ src/stories/atoms/NbosMetricsToggle.stories.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { NbosDashboard } from '../../components/pages/NbosDashboard';
+import { NbosMetricsToggle } from '../../components/atoms/NbosMetricsToggle';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'templates/NbosDashboard',
-  component: NbosDashboard,
+  title: 'Atoms/NbosMetricsToggle',
+  component: NbosMetricsToggle,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -13,7 +13,7 @@ export default {
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = args => <NbosDashboard {...args} />;
+const Template = args => <NbosMetricsToggle {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
